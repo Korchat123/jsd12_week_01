5650e117addb9a459b135636efd1495690f4101c
Korchat123 <korchat123@gmail.com>
2026-03-06 13:56:49 +0700

    add js and note of today lerning

---
+++ object-and-array.js
@@ -0,0 +1,11 @@
+let learner={
+    name:"Korchat",
+    age:33,
+    isgraduated:false,
+    hobbies:["watch Vtuber","watch anime","watch Netfilx","Play game"]
+
+};
+console.log(learner.name);
+console.log(learner.age);
+console.log(learner.isgraduated);
+console.log(learner.hobbies[0]);
